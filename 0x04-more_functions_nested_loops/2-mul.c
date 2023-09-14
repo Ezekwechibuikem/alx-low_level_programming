eb487ce6c99be353c233870073d1a9d2b9aeea0f
ezekwe chibuikem <Nwachinaemelum@yahoo.com>
2023-09-14 06:32:32 -0700

    2 mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - multiplying two int
+ * @c: function parameter 1
+ * @b: function parameter 2
+ * Return: return void
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+
+}
