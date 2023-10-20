6b301fbe15563426982a943891606f252d20df94
ezekwe chibuikem <Nwachinaemelum@yahoo.com>
2023-10-19 19:44:00 -0700

    trialing whitespace corrected

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -5,8 +5,8 @@
  * list_len - Returning the number of elements in a linked list
  * @h: pointer to the list_t list inside the function
  *
- * 
- * Return: returns the number of elements in h
+ *
+ * * Return: returns the number of elements in h
  */
 size_t list_len(const list_t *h)
 {
