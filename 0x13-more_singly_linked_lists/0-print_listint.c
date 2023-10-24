95aba070bdb9e257a7295b5a0063d2b77fc26af9
ezekwe chibuikem <Nwachinaemelum@yahoo.com>
2023-10-24 11:06:08 -0700

    zero man

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 
 /**
- * print_listint - prints all the elements of a linked list
- * @h: linked list of type listint_t to print
- *
- * Return: number of nodes
+ * print_listint - fun
+ * @h: linked list 
+ * 
+ * Return: number
  */
 size_t print_listint(const listint_t *h)
 {
@@ -12,9 +12,9 @@ size_t print_listint(const listint_t *h)
 
 	while (h)
 	{
-	printf("%d\n", h->n);
-	number++;
-	h = h->next;
+		printf("%d\n", h->n);
+		h = h->next;
+		number++;
 	}
 
 	return (number);
